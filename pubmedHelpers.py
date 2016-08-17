3682d2c4c8a3e495a41fe7011f3c328feb6c577d
Darkbladecr <smig88@gmail.com>
2016-08-16 21:21:30 -0700

    PEP 8 compliant

--- pubmedHelpers.py
+++ pubmedHelpers.py
@@ -6,6 +6,7 @@ from Bio import Entrez
 from iso639 import languages
 from tqdm import tqdm
 
+
 def excel2Dict(filename):
 	wb = load_workbook(filename=filename, read_only=True)
 	ws = wb.active
@@ -25,6 +26,7 @@ def excel2Dict(filename):
 			count += 1
 	return records
 
+
 def saveExcel(filename, title, data):
 	wb = Workbook()
 	ws = wb.active
@@ -39,6 +41,7 @@ def saveExcel(filename, title, data):
 	date = datetime.now().strftime('%Y-%m-%d')
 	name = "%s-%s.xlsx" % (filename, date)
 	wb.save(name)
+
 
 def saveWorksheet(wb, title, data, searchTerm=None, orderedEntries=None):
 	ws = wb.create_sheet()
@@ -62,6 +65,7 @@ def saveWorksheet(wb, title, data, searchTerm=None, orderedEntries=None):
 				output.append(val)
 		ws.append(output)
 
+
 def distinctSet(records, title):
 	data = Set([])
 	for record in records:
@@ -72,12 +76,14 @@ def distinctSet(records, title):
 			data.add(record[title])
 	return data
 
+
 def languageParse(record):
 	if len(record['LA']) > 1:
 		return [languages.get(part2b=l.lower()).name for l in record['LA']]
 	else:
 		langCode = record['LA'][0].lower()
 		return languages.get(part2b=langCode).name
+
 
 def outputYearlyData(records):
 	output = dict()
@@ -89,8 +95,10 @@ def outputYearlyData(records):
 			output[year] = 1
 	return output
 PToutput = ['ENGLISH ABSTRACT', 'Meta-Analysis', 'Controlled Clinical Trial', 'LETTER', 'Clinical Trial, Phase III', 'Review', "Research Support, U.S. Gov't, P.H.S.", 'Guideline', 'Interview', "Research Support, Non-U.S. Gov't", 'Consensus Development Conference', 'Lectures', "Research Support, U.S. Gov't, Non-P.H.S.", 'Published Erratum', 'Clinical Study', 'Overall', 'REVIEW', 'Letter', 'Observational Study', 'Comparative Study', 'Clinical Trial, Phase I', 'Comment', 'Multicenter Study', 'Validation Studies', 'Journal Article', 'JOURNAL ARTICLE', 'Historical Article', 'Evaluation Studies', 'Research Support, N.I.H., Extramural', 'Editorial', 'Retracted Publication', 'CASE REPORTS', 'Classical Article', 'Technical Report', 'Randomized Controlled Trial', 'Video-Audio Media', 'English Abstract', 'News', 'Portraits', 'Clinical Trial, Phase II', 'Research Support, N.I.H., Intramural', 'Introductory Journal Article', 'Congresses', 'Case Reports', 'Practice Guideline', 'Clinical Trial', 'Biography']
-PTwant = ["Journal Article","Letter","Editorial","Review","Meta-Analysis","Research Support, Non-U.S. Gov't","Research Support, U.S. Gov't, P.H.S.","Research Support, U.S. Gov't, Non-P.H.S.","Research Support, N.I.H., Extramural","Research Support, N.I.H., Intramural","Clinical Study","Observational Study","Comparative Study","Multicenter Study","Validation Studies","Controlled Clinical Trial","Clinical Trial","Clinical Trial, Phase I","Clinical Trial, Phase II","Clinical Trial, Phase III","Randomized Controlled Trial","Guideline","Practice Guideline","Lectures","Case Reports","Historical Article","Evaluation Studies"]
+PTwant = ["Journal Article", "Letter", "Editorial", "Review", "Meta-Analysis", "Research Support, Non-U.S. Gov't", "Research Support, U.S. Gov't, P.H.S.", "Research Support, U.S. Gov't, Non-P.H.S.", "Research Support, N.I.H., Extramural", "Research Support, N.I.H., Intramural", "Clinical Study", "Observational Study", "Comparative Study", "Multicenter Study", "Validation Studies", "Controlled Clinical Trial", "Clinical Trial", "Clinical Trial, Phase I", "Clinical Trial, Phase II", "Clinical Trial, Phase III", "Randomized Controlled Trial", "Guideline", "Practice Guideline", "Lectures", "Case Reports", "Historical Article", "Evaluation Studies"]
 PTdel = ['ENGLISH ABSTRACT', 'LETTER', 'Interview', 'Published Erratum', 'Overall', 'REVIEW', 'Comment', 'JOURNAL ARTICLE', 'CASE REPORTS', 'Classical Article', 'Technical Report', 'Video-Audio Media', 'English Abstract', 'News', 'Portraits', 'Introductory Journal Article']
+
+
 def outputPubDataPubmed(records):
 	output = dict()
 	renamer = {'LETTER': 'Letter', 'REVIEW': 'Review', 'JOURNAL ARTICLE': 'Journal Article', 'CASE REPORTS': 'Case Reports'}
@@ -106,6 +114,8 @@ def outputPubDataPubmed(records):
 	return OrderedDict((k, output[k]) for k in PTwant)
 
 PTscopus = ["Article", "Letter", "Editorial", "Review", "Conference Paper", "Chapter", "Note", "Short Survey"]
+
+
 def outputPubDataScopus(records):
 	output = OrderedDict((k, 0) for k in PTscopus)
 	for r in records:
@@ -113,10 +123,12 @@ def outputPubDataScopus(records):
 		output[pubType] += 1
 	return output
 
+
 def getPubmedIds(term, start=0):
 	handle = Entrez.esearch(db="pubmed", retstart=start, retmax=20000, term=term)
 	record = Entrez.read(handle)
 	return record["IdList"]
+
 
 def parsePubmed(records, total, array, pmids):
 	for r in tqdm(records):
@@ -137,6 +149,7 @@ def parsePubmed(records, total, array, pmids):
 				array.append(pub)
 				pmids.append(r['PMID'])
 
+
 def cyberVgamma(records, searchTerm):
 	cyberPubs = list()
 	gammaPubs = list()
@@ -148,4 +161,3 @@ def cyberVgamma(records, searchTerm):
 		if record['PMID'] in gammaIds:
 			gammaPubs.append(record)
 	return {'cyber': cyberPubs, 'gamma': gammaPubs}
-
